f2c1d78f14cda686976e7c01534c9f244f947a62
Danbi Choi <sweetrain05@hotmail.com>
2024-01-28 22:08:45 -0800

    Intallation & Selecting & Implicit and explicit assertions

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,12 @@ import './Header.css';
 import logo from '../assets/logo.png';
 
 function Header() {
-  return (
-    <header className="main-header">
-      <img src={logo} alt="A list" />
-      <h1>React Tasks</h1>
-    </header>
-  );
+    return (
+        <header className='main-header'>
+            <img src={logo} alt='A list' />
+            <h1>My Cypress Course Tasks</h1>
+        </header>
+    );
 }
 
 export default Header;
